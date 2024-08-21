9c0e9753338c4f298151ec8e04f4150e11851e5c
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-21 21:40:27 +0530

    chore: express added

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ dotenv.config();
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API STARTED");
+});
+
+app.listen(PORT, console.log(`App running on port ${PORT}`));
