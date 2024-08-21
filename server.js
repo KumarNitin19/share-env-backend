7f8644c32cbf0067c9cf4e7b106d137a24739d49
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-21 22:33:26 +0530

    fix: ui fixes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("API STARTED");
 });
