6a532d63d637a35b3d0fa2a20d2af3f84477ff41
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-26 22:14:26 +0530

    feat: auth middlware added

---
+++ middleware/privateRoute.middleware.js
@@ -0,0 +1,14 @@
+const authenticateUser = (req, res, next) => {
+  const idToken = req.headers.authorization;
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      next();
+    })
+    .catch((error) => {
+      console.log("Error verifying ID token:", error);
+      res.status(401).json({ error: "Invalid token" });
+    });
+};
