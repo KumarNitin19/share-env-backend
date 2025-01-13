7e8ca927fd6e76ac05c3c1231eaae00689035583
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-13 22:54:22 +0530

    fix: few secerets added using crypto

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const cliRoutes = require("./routes/cli-routes");
-const projectRoutes = require("./routes/project");
+const crypto = require("crypto");
+// const cors = require("cors");
+// const cliRoutes = require("./routes/cli-routes");
+// const projectRoutes = require("./routes/project");
 
 dotenv.config();
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 
-app.use(cors());
-app.use("/cli", cliRoutes);
-app.use("/app/v1", projectRoutes);
-app.listen(PORT, console.log(`App running on port ${PORT}`));
+// app.use(cors());
+// app.use("/cli", cliRoutes);
+// app.use("/app/v1", projectRoutes);
+// app.listen(PORT, console.log(`App running on port ${PORT}`));
+
+console.log(crypto.randomBytes(64).toString("base64"));
