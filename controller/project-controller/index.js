20de0f32b5c7bebbc0ab25004e20b5ae4720c579
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-22 21:52:33 +0530

    feat: get project api created

--- controller/project-controller/index.js
+++ controller/project-controller/index.js
@@ -10,12 +10,26 @@ const deleteProject = asyncHandler((req, res) => {});
 
 const getAllProjects = asyncHandler(async (req, res) => {
   const uid = await getUserUid(req);
-  const projects = await db.collection("projects").doc(uid).get();
-  console.log(projects);
-  res.status(200).json({
-    message: "No project found",
-    data: [],
-  });
+  try {
+    const projectsRef = db.collection("projects");
+    const querySnapshot = await projectsRef.where("uid", "==", uid).get();
+
+    if (querySnapshot.empty) {
+      return res
+        .status(404)
+        .json({ error: "No projects found for this user." });
+    }
+
+    const projects = querySnapshot.docs.map((doc) => ({
+      projectId: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = { createProject, editProject, deleteProject, getAllProjects };
