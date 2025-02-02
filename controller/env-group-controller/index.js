5f427a89f7115c0f29caf1e7dfd54bba5a864fc4
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-02 21:18:01 +0530

    fix: get env group api fix

--- controller/env-group-controller/index.js
+++ controller/env-group-controller/index.js
@@ -56,9 +56,7 @@ const getENVGroups = asyncHandler(async (req, res) => {
 
     // Check if any groups exist
     if (groupsSnapshot.empty) {
-      return res
-        .status(404)
-        .json({ error: "No groups found for this project." });
+      return res.status(200).json({ projectId, groups: [] });
     }
 
     // Extract group data
