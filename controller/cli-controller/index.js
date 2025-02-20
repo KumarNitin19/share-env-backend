92e06ba1171011b886f476e9ab1b8a12af618e2f
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-20 23:45:07 +0530

    fix: getallenvvariable function fixes

--- controller/cli-controller/index.js
+++ controller/cli-controller/index.js
@@ -21,29 +21,41 @@ const getAllENVVariables = asyncHandler(async (req, res) => {
     const projectRef = db.collection("projects").doc(projectId);
     const projectDoc = await projectRef.get();
 
-    console.log(projectDoc);
-    // Query groups where projectId matches
-    const groupsSnapshot = await db
-      .collection("groups")
-      .where("projectId", "==", projectId)
-      .get();
-
-    // Check if any groups exist
-    if (groupsSnapshot.empty) {
-      return res.status(200).json({ projectId, groups: [] });
+    // Check if the project exists
+    if (!projectDoc.exists) {
+      return res.status(404).json({ error: "Project not found." });
     }
 
-    // Extract group data
-    const groups = groupsSnapshot.docs.map((doc) => ({
-      groupId: doc.id,
-      ...doc.data(),
-    }));
+    const projectData = projectDoc.data();
 
-    // Return response
-    res.status(200).json({
-      projectId,
-      groups,
-    });
+    if (projectData?.projectMembers.includes(githubUserName)) {
+      // Query groups where projectId matches
+      const groupsSnapshot = await db
+        .collection("groups")
+        .where("projectId", "==", projectId)
+        .get();
+
+      // Check if any groups exist
+      if (groupsSnapshot.empty) {
+        return res.status(200).json({ projectId, groups: [] });
+      }
+
+      // Extract group data
+      const groups = groupsSnapshot.docs.map((doc) => ({
+        groupId: doc.id,
+        ...doc.data(),
+      }));
+
+      // Return response
+      res.status(200).json({
+        projectId,
+        groups,
+      });
+    } else {
+      res.status(403).json({
+        error: "You're not authorized/collaborator of this project!!",
+      });
+    }
   } catch (error) {
     console.error("Error fetching groups:", error);
     res.status(500).json({ error: "Internal Server Error" });
