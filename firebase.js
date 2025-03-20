9c17d6395b86219d892189760f0f68bbb10f4ca9
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-03-20 22:47:33 +0530

    fix: firebase configuations added

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const config = require("./serviceAccountKey.json");
+const config = require("./config");
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
