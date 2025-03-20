6739147e4c82bf30adaec3c6fc0cf0b85093b7ee
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-03-20 22:47:12 +0530

    fix: config file fixed

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
-import dotenv from "dotenv";
-import assert from "assert";
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const {
   PORT,
-  HOST,
-  HOST_URL,
-  API_KEY,
-  AUTH_DOMAIN,
+  DATABASE_URI,
   PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
+  PRIVATE_KEY_ID,
+  PRIVATE_KEY,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+  AUTH_URI,
+  TOKEN_URI,
+  AUTH_PROVIDER_URI,
+  CLIENT_URI,
 } = process.env;
 
-assert(PORT, "Port is required");
-assert(HOST, "Host is required");
-
-export default {
+module.exports = {
   port: PORT,
-  host: HOST,
-  hostUrl: HOST_URL,
   firebaseConfig: {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
+    project_id: PROJECT_ID,
+    private_key_id: PRIVATE_KEY_ID,
+    private_key: PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: CLIENT_EMAIL,
+    client_id: CLIENT_ID,
+    auth_uri: AUTH_URI,
+    token_uri: TOKEN_URI,
+    auth_provider_x509_cert_url: AUTH_PROVIDER_URI,
+    client_x509_cert_url: CLIENT_URI,
   },
+  database_uri: DATABASE_URI,
 };
